c21630109fc10c6136482013997b26a062d2ef7f
Ana Carolina Cebin Pereira <31068888+CarolCebin@users.noreply.github.com>
2017-11-05 23:27:00 -0200

    Data
    Adicionando as Buscas com a função data

--- arquivos/9.4/Busca9.4.sql
+++ arquivos/9.4/Busca9.4.sql
@@ -1,3 +1,4 @@
+
 /*Exemplo 9.4.1*/
 SELECT * FROM filme WHERE sinopse LIKE '%Star%' or  subtitulo LIKE '%trevas%'; /*Procura Filmes que contenha 
 a palavra Star no titulo ou a palavra Trevas no subtitulo*/
@@ -17,4 +18,25 @@ SELECT * FROM usuario WHERE email LIKE '%gmail%'; /*Procura os ususario com emai
 
 /*Exemplo 9.4.6*/
 SELECT * FROM cep WHERE endereco LIKE '%Rua%'; /*Procura os ususario que moram em uma Rua*/
+
+/*Exemplo 9.4.7*/
+Select nome, 
+current_date as data_atual, 
+ultimo_acesso, 
+(age(current_date, ultimo_acesso))
+FROM usuario; /*Calcula o tempo do ultimo acesso (Com a funcao AGE)*/
+
+
+/*Exemplo 9.4.8*/
+Select id_usuario, 
+current_date as data_atual, 
+prox_pgto, 
+date_part('day', age(prox_pgto, current_date)) as tempoValidade
+FROM pagamento; /*Calcula os dias restante da Assinatura (Com a funcao AGE e DATE_PART)*/
+
+/*Exemplo 9.4.9*/
+Select id_usuario, 
+current_date as data_atual, ultm_pgto, 
+extract('day' from ultm_pgto)
+FROM pagamento; /*Dia do mes que sempre ocorrera o vencimento da Assinatura*/
 
